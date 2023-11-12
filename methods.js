8903acf64beb7155568eeda646b9dc32b884375a
niktos12 <nikitosss121207@gmail.com>
2023-11-12 19:18:26 +0400

    fix and...

--- methods.js
+++ methods.js
@@ -16,14 +16,11 @@ const people = [
         age: 30
     }
 ]
-const filteredAge = people.filter(function (person){
-    return person.age > 30  // будет выводить только тех людей возраст которых больше 30  
-})
+const filteredAge = people.filter((person) => (person.age > 30));  // будет выводить только тех людей возраст которых больше 30  
+
 // console.log(filteredAge);    //    [ { name: 'Alex', age: 35 } ]
 
-const namesPerson = people.map(function (person){
-    return person.name;  // выводим имя каждого человека
-})
+const namesPerson = people.map((person) => (person.name));  // выводим имя каждого человека
 // console.log(namesPerson);  // [ 'Bob', 'Alex', 'Jane', 'Kate' ]
 
 const forEachPeople = people.forEach(function(person){    
@@ -34,10 +31,8 @@ const forEachPeople = people.forEach(function(person){
   { name: 'Alex', age: 35 }
   { name: 'Jane', age: 25 }
   { name: 'Kate', age: 30 } */
-const reduceAges = people.reduce(function(total, person){
-    return total + person.age;  // считает сумму всех возрастов
-}, 0) // начальное значение total ставим на 0
-// console.log(reduceAges);   // 110
+const reduceAges = people.reduce((total, person) => total + person.age, 0);  // считает сумму всех возрастов // начальное значение total ставим на 0
+console.log(reduceAges);   // 110
 function forPeople(people){
     for(let i = 0; i < people.length; i++){      // проход по массиву
         console.log(people[i])  // выводим каждого человека
@@ -52,18 +47,56 @@ function forPeople(people){
 let randomNumber;
 let arrayNumbersLessThan500 = [];
     
-function fullRandom(){
-    while (randomNumber < 500 || randomNumber == undefined){ // пока случайное число меньше 500 или равно undefined
-        randomNumber = Math.round(Math.random() * 1001);
-        if(randomNumber < 500){
+function fullRandom(min, max, minNumberForEnd){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    while (randomNumber < minNumberForEnd || randomNumber == undefined){ // пока случайное число меньше 500
+        randomNumber = Math.round(min + Math.random() * (max + 1 - min)) + min;
+        if(randomNumber < minNumberForEnd){
             arrayNumbersLessThan500.push(randomNumber); // если рандомное число меньше 500, то добавляем его в массив
         }
     }
     return arrayNumbersLessThan500; // в конце возвращаем массив
 }
     
-console.log(fullRandom());  
-// firts try : [226]
-// second try : [66, 439]
-// third try :  [ 289, 95, 115, 46, 235, 220 ]
+console.log(fullRandom(0,1000,500));
+// firts try : [ 193, 478 ]
+// second try : [ 197 ]
+// third try :  []
+
+
+const field = [["","",""],["","",""],["","",""]];
+let fillFieldInterval;
+let intervalLogField;
+
+function fillField(){
+    let isFilled = true;
+    for(let i = 0; i < 3; i++){
+        for(let j = 0; j < 3; j++){
+            isFilled = true;
+            if(field[i][j] === ''){
+                field[i][j] = Math.random() < 0.5 ? 'X' : 'O';
+                isFilled = false;
+                return;
+            }else{
+                isFilled = true;
+            }
+        }
+    }
+    if(isFilled === true) {
+        clearInterval(fillFieldInterval); // Останавливаем функцию fillField
+        clearInterval(intervalLogField); // Останавливаем функцию logField(т.е. не выводим в консоль)
+        console.log('field filled');
+}
+
+
+}
+
+function logField() {
+    console.log(field);
+}
+fillFieldInterval = setInterval(fillField, 1000); // Запускаем функцию fillField каждую секунду
+intervalLogField = setInterval(logField, 1000);
+
+
 
